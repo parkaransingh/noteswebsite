ce940e1b1c5fb308f2d6d2679b4d92b13ad76bd4
Parkaranjot Singh <parkaranjot@Parkaranjots-MacBook-Pro.local>
2021-12-26 22:29:10 -0500

    HTML page templates

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 #main views or url endpoints for front end aspect of our website
-from flask import Blueprint
+from flask import Blueprint, render_template
 # this has a bunch of routes inside this file
 views = Blueprint('views', __name__)
 
 @views.route('/') # go slash main page will run -> this line is called the decorator
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
