d51a5e9896ef19b00ec4e70cbe6cb4048521a89d
Parkaranjot Singh <parkaranjot@Parkaranjots-MacBook-Pro.local>
2021-12-28 18:43:11 -0500

    login and signup form checks

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 # make website folder a python package, we can import website folder and whatever is in this init.py file, it can run automatically when we iimport 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() # object to manipulate the database
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__) # __name represents the name of the file
     app.config['SECRET_KEY'] = 'confidence123' # secret key for encryption of cookies and session data related to our website
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # store this database inside the website folder
+
     from .views import views
     from .auth import auth
     app.register_blueprint(views, url_prefix= '/')
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,36 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
-
-@auth.route('/login')
+# get is retrieving information 
+# post change to state of the system or database ex post request of signing in
+# update, delete, put
+@auth.route('/login', methods=['GET', 'POST']) # did we send a get or post request, GOING TO URL IS GET REQUEST, POST REQUEST IS THE SUBMISSION OF LOGIN BUTTON 
 def login():
-    return render_template("login.html")
-
+    # data = request.form # request variable inside a root will have information of the request sent to access this route, we can access the form data using .form
+    # print(data) # will print a immutatable multi Dict
+    return render_template("login.html", boolean = True)
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    # differentiate get and post request 
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # few python checks with message flashing
+        if len(email) < 4:
+            flash('Email must be greater than or equal to 4 characters.', category = 'error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than or equal to 2 characters.', category = 'error')
+        elif password1 != password2:
+            flash('Passwords do not match', category = 'error')
+        elif len(password1) < 7:
+            flash('Passwords must be greater than or equal to 7 characters.', category = 'error')
+        else:
+            flash('Account created!', category = 'success')
     return render_template("sign_up.html")
